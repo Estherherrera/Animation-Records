cad49c4cf60b9f92f97bc6fa01e761a4872d50a1
Estherherrera <graxyherrera@gmail.com>
2022-05-26 19:38:11 +0200

    agregando el js de miniature vs Principal

---
+++ public/js/miniature.js
@@ -0,0 +1,31 @@
+function showEarrings() {
+    let earrings = document.getElementById('earrings')
+    console.log(earrings.id)
+        //let small = document.getElementById('aretes2ProductImage')
+
+    earrings.src = "/public/assets/photos/productos/Aretes.avif";
+    // small.src = "/public/assets/photos/productos/Aretes.avif";
+}
+
+function showPendant() {
+    let earrings = document.getElementById('earrings')
+    console.log(earrings.id)
+        //let small = document.getElementById('pendienteProductImage')
+        //console.log(small.id)
+
+
+    //if (big.id == "big" && small.id == "pendienteProductImage") {
+    earrings.src = "/public/assets/photos/productos/pendiente.png";
+    //small.src = "/public/assets/photos/productos/Aretes.avif";
+    // }
+
+}
+
+function showNecklace() {
+    let earrings = document.getElementById('earrings')
+    console.log(earrings.id)
+        //let small = document.getElementById('collaresProductImage')
+
+    earrings.src = "/public/assets/photos/productos/collares.png";
+    //small.src = "/public/assets/photos/productos/Aretes.avif";
+}
